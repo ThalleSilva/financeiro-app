009f110baa4353f449082065d97a17495bab26e6
Thalles <167459991+ThalleSilva@users.noreply.github.com>
2025-06-30 14:19:45 -0300

    db.py

--- db.py
+++ db.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
+
+def init_app(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
